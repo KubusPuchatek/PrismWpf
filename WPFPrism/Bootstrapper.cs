81b4f68c653a1690a30115b5434fbe77d2c628c3
Łukasz Doszczeczko <ldos@dgs.com>
2019-09-09 14:01:51 +0200

    Initial Shell configuration

---
+++ WPFPrism/Bootstrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Mef;
+
+namespace WPFPrism
+{
+    public class Bootstrapper : MefBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.GetExportedValue<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Application.Current.MainWindow = (Window) Shell;
+            Application.Current.MainWindow?.Show();
+        }
+
+        protected override void ConfigureAggregateCatalog()
+        {
+            base.ConfigureAggregateCatalog();
+
+            AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(Bootstrapper).Assembly));
+        }
+    }
+}
